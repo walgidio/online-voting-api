41f49c34f5671cf32142e68358ed4fe366bb72c2
Walgídio Santos <walgidio.s@gmail.com>
2025-06-30 18:01:15 +0100

    refactor

--- src/main/java/com/voting/voting_app/controllers/PollController.java
+++ src/main/java/com/voting/voting_app/controllers/PollController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class PollController {
     private final PollService pollService;
 
--- src/main/java/com/voting/voting_app/models/VoteOption.java
+++ src/main/java/com/voting/voting_app/models/VoteOption.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Embeddable
 public class VoteOption {
-    private String voteOption;
+    private String optionText;
     private Long voteCount = 0L;
 }
